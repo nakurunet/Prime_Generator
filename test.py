de47dc39640a5573d0f6a71799f08bc2a40cffc8
Philip On <nakurunet@gmail.com>
2016-12-01 01:28:12 +0300

    update

---
+++ test.py
@@ -0,0 +1,25 @@
+import unittest
+from prime_function import isPrime
+
+class PrimeTests(unittest.TestCase):
+	
+	def test_input_must_not_be_float(self):
+		self.assertEqual(isPrime(10.5),"The number must be a whole number")
+	
+	def test_no_string(self):
+		self.assertEqual(isPrime("a"), "Cannot allow letters")
+
+	def test_no_list(self):
+		self.assertEqual(isPrime([24]), "Cannot allow lists")
+	
+	def test_input_must_not_be_a_tuple(self):
+		self.assertEqual(isPrime((100)),"The number must be a whole number")
+	
+	def test_not_big_integer(self):
+		self.assertTrue(isPrime(isPrime>1000),'large numbers not allowed!!!')
+	
+	def test_input_must_not_be_a_boolean(self):
+		self.assertEqual(isPrime(True),"The number must not be a boolean")
+
+if __name__ == 'main':
+	unittest.main()
